0533fddd8e1b9e5722fc59716c61b378fd07ce78
bex-Machina <bex-Machina@users.noreply.github.com>
2022-06-10 23:57:14 +0100

    Simple ray cast demo

---
+++ Raycasting/Assets/placer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placer : MonoBehaviour
+{
+
+    public Transform cube;
+    public new Camera camera;
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo;
+        if (Physics.Raycast(ray, out hitinfo))
+        {
+            cube.position = hitinfo.point;
+        }
+    }
+}
